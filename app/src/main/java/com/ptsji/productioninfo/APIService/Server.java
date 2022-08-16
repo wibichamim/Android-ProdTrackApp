3c3100a6457af802e5a0a197cbf46db841e96ce0
Wibi Chamim M <33890271+wibichamim@users.noreply.github.com>
2022-08-16 09:43:45 +0700

    Update Server.java

--- app/src/main/java/com/ptsji/productioninfo/APIService/Server.java
+++ app/src/main/java/com/ptsji/productioninfo/APIService/Server.java
@@ -1,6 +1,6 @@
 package com.ptsji.productioninfo.APIService;
 
 public class Server {
-    public static final String URL = "http://api.ptsji.id/api-production/";
-    public static final String URL1 = "http://api.ptsji.id/api-machine-movement/";
+    public static final String URL = "-";
+    public static final String URL1 = "-";
 }
